be1d050452a840d08e175d8770e4ee3d0cfc69b3
Jenny Yang <michel3153@gmail.com>
2020-09-04 10:06:16 +0100

    Initial commit of my components without tests

---
+++ src/component/header/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./styles.scss"
+import Logo from "./../../assets/graphics/logo.png"
+
+const Header = (props) => {
+  return (
+    <header data-test="headerComponent">
+      <div className="wrap">
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+      </div>
+    </header>
+  )
+}
+export default Header
